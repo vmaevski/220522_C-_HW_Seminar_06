99a60285c6175791225db186697b557e2ecadf4d
vmaevski <vmaevski@mail.ru>
2022-05-29 13:56:15 +0700

    Thinking about Task 1...

---
+++ 220528_Task_1/Program.cs
@@ -0,0 +1,25 @@
+﻿// 1.Пользователь вводит с клавиатуры M чисел. 
+// Посчитайте, сколько чисел больше 0 ввёл пользователь.
+
+//Entering and counting numbers
+
+// int EnteringNumbers(int count){
+void CountingPositivNumbers(int amount)
+{
+    string str = "Введееные числа: ";
+    double num = 0;
+    int amountPositiv = 0;
+    for (int i = 1; i <= amount; i++)
+    {
+        Console.WriteLine($"Введите число номер {i}:");
+        num = Console.ReadLine().;
+        if (num > 0)
+        {
+            amountPositiv++;
+        }
+        str += ($" {Math.Round(num, 0).ToString()}");
+    }
+    Console.WriteLine(str);
+}
+
+CountingPositivNumbers(10);
